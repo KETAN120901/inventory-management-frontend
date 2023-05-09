ed72c92a8f93d09013a6c62df284b45576f4235e
Ketan Chouhan <ketan120901@gmail.com>
2023-05-09 12:32:56 +0530

    changge12

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
  function Item(props){
     function handleClick(){
         props.onDelete(props.id);
 
     }
     var path ="items/"+props.name;
-    return <div className="item-box"><a href={path} ><div>Item-Name : {props.name}</div><div>Price : {props.price}</div><div>Quantity : {props.quantity}</div></a></div>
+    return <div className="item-box"><Link to={path} ><div>Item-Name : {props.name}</div><div>Price : {props.price}</div><div>Quantity : {props.quantity}</div></Link></div>
  }
  export default Item;
