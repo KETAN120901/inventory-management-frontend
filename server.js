54f022132cc96c741c1cb40a69e3211947647915
Ketan Chouhan <ketan120901@gmail.com>
2023-05-19 09:09:32 +0530

    build9

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
@@ -5,13 +6,13 @@ const app = express();
 
 // Serve static files from the 'build' directory
 app.use(express.static(path.join(__dirname, 'build')));
-
+const PORT=process.env.PORT || 3000;
 // Catch-all route that serves the 'index.html' file
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}"`);
+  });
